65cbfe96aa5c14d864433992ef6fb872816f44d6
Soumyajit Roy <soumyajitroy2411@gmail.com>
2021-09-14 21:08:40 +0530

    pro

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
+let ejs = require("ejs");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
 });
 
 app.listen(port, () => {
