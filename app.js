2327e3cbb76ced379bbfa3af8e66f8244ae71566
Soumyajit Roy <soumyajitroy2411@gmail.com>
2021-09-23 02:58:32 +0530

    pro

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 let ejs = require("ejs");
+const Razorpay = require("razorpay");
+
+var instance = new Razorpay({
+  key_id: "rzp_test_h60mCWCH1om22f",
+  key_secret: "KfVU4VOud16dNm9UvsKs8mb9",
+});
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +28,27 @@ app.get("/contact", (req, res) => {
 
 app.get("/donation", (req, res) => {
   res.render("donation");
+});
+
+app.post("/order", (req, res) => {
+  var options = {
+    amount: 50000,
+    currency: "INR",
+  };
+  instance.orders.create(options, function (err, order) {
+    console.log(order);
+    res.json(order);
+  });
+});
+
+app.post("/js-order-complete", (req, res) => {
+  instance.payments.fetch(req.body.razorpay_payment_id).then((docs) => {
+    if (docs.status == "captured") {
+      res.send("Payment successful");
+    } else {
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(port, () => {
