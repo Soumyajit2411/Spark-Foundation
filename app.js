28f4bff12953080cc8efa39fac353f57c17ced4b
Soumyajit Roy <soumyajitroy2411@gmail.com>
2021-09-26 21:06:55 +0530

    p

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 const mongoose = require("mongoose");
 let ejs = require("ejs");
 
